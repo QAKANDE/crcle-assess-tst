c1e7185b429eb1e0dcb07c608a1393cb8e13ba9f
QOA <58850207+QAKANDE@users.noreply.github.com>
2024-11-06 22:04:16 +0000

    push

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const server = express();
-const walletRouter = require("./Routes/wallet.route");
 const eventRouter = require("./Routes/event.route");
 const port = process.env.PORT || 3000;
 
